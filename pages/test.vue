b3341e0d7f92c63bde23598998d8669deb513f50
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-03 22:31:23 +0800

    update: add test file

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class=" min-h-screen bg-black">
+        <h1 class="text-white">Test</h1>
+        <p>Test page</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.vue',
+    './components/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
