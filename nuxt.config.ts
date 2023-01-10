b424785d70309d9c7db1bd241703dbef0487932d
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-11 00:21:54 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,40 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
+      title: "Anime Nuxt App - ANA",
       titleTemplate: "%s - ANA",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Full list of the Most Popular and Must Watch Anime Online Free, with SUBBED and DUBBED. WATCH NOW!",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Anime Nuxt App - ANA",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "Full list of the Most Popular and Must Watch Anime Online Free, with SUBBED and DUBBED. WATCH NOW!",
+        },
+        {
+          hid: "og:image",
+          property: "og:image",
+          content:
+            "https://img.zorores.com/_r/300x400/100/bc/a4/bca4a71e271a30fb1b44d33d56db8f8f/bca4a71e271a30fb1b44d33d56db8f8f.jpg",
+        },
+        {
+          hid: "robots",
+          name: "robots",
+          content: "index, follow",
+        },
+      ],
     },
   },
   runtimeConfig: {
