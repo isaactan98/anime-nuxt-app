c8b085000f02898d5a131a4d764c0835e807d1c7
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-11 23:48:44 +0800

    test: video player

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,10 +1,55 @@
 <template>
-  
+    <div class=" bg-white min-h-screen">
+        <video ref="videoPlayer" class="video-js" id="video_player"></video>
+
+        <video v-if="url != ''" :src="url.url"></video>
+    </div>
 </template>
 
 <script>
-export default {
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css'
 
+export default {
+    props: ['videoDetails'],
+    data() {
+        return {
+            video: null,
+            url: ""
+        }
+    },
+    mounted() {
+        const details = { ...this.videoDetails }
+
+        if (this.videoDetails.sources) {
+            this.url = { ...details.sources[0] }
+
+            var src = this.url.url ?? 'https://www.w3schools.com/html/mov_bbb.mp4'
+
+            this.video = videojs('video_player', {
+                controls: true,
+                autoplay: false,
+                preload: 'auto',
+                fluid: true,
+                sources: [{
+                    src: src,
+                }],
+                // track: [{
+                //     kind: 'captions',
+                //     src: 'https://www.w3schools.com/html/mov_bbb.vtt',
+                //     srclang: 'en',
+                //     label: 'English'
+                // }]
+            }, () => {
+                this.video.log('onPlayerReady', this);
+            });
+        }
+    },
+    beforeDestroy() {
+        if (this.video) {
+            this.video.dispose();
+        }
+    }
 }
 </script>
 
--- pages/animes/watch/[watch].vue
+++ pages/animes/watch/[watch].vue
@@ -1,17 +1,37 @@
 <template>
     <div>
         <!-- video player -->
-
+        <VideoPlayer v-if="video" :videoDetails="video"></VideoPlayer>
+        <div v-else class="text-white">
+            Loading...
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 export default {
+    data() {
+        return {
+            video: null
+        }
+    },
     mounted() {
         const route = useRoute();
+        const config = useRuntimeConfig();
         var id = route.params.watch;
 
-        console.log(id)
+        fetch(`${config.apiUrl2}watch?episodeId=${id}`)
+            .then(res => {
+                // console.log('res', res)
+                return res.json()
+            })
+            .then(data => {
+                // console.log('data', JSON.parse(JSON.stringify(data)))
+                // unproxy data 
+                this.video = JSON.parse(JSON.stringify(data));
+                console.log('video', this.video)
+            }).catch(err => console.log(err))
+
     },
 }
 </script>
