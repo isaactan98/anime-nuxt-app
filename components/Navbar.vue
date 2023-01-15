2da3dbce337f779adf68ef0f0a5623397f9736a7
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-15 22:49:08 +0800

    update: search function

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,12 +5,37 @@
                 <li class="text-white">
                     <button @click="toggleSidebar()" class="px-2 bg-white rounded-md text-black">=</button>
                 </li>
-                <li>
+                <li class="flex items-center gap-3">
+                    <button @click="toggleSearch" v-show="!showSearch">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            class="w-6 h-6 text-white font-extrabold">
+                            <path fillRule="evenodd"
+                                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </button>
+
                     <ProfileDropdown v-if="userId || checkUserId"></ProfileDropdown>
                     <LoginSignUpModal v-else></LoginSignUpModal>
                 </li>
             </ul>
         </nav>
+        <!-- search input -->
+        <div class="absolute w-full" v-show="showSearch">
+            <div class="flex relative justify-center container mx-auto">
+                <input type="text" name="price" id="price"
+                    class="block w-full rounded-md border-gray-300 pl-7 pr-12 py-3 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Search anime ..." v-model="searchQuery">
+                <button class="absolute inset-y-0 right-3 flex items-center p-3" @click="searchAnime">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                        class="w-6 h-6 font-extrabold">
+                        <path fillRule="evenodd"
+                            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                            clipRule="evenodd" />
+                    </svg>
+                </button>
+            </div>
+        </div>
         <!-- collapse sidebar -->
         <div class="w-full min-h-screen absolute overflow-hidden z-50 hidden">
             <div class="bg-gray-800 w-3/4 lg:w-1/5" id="collapse-sidebar">
@@ -39,7 +64,9 @@ export default {
             sidebar_item: [
                 { id: 'home', name: 'Home', route: '/' },
             ],
-            checkUserId: ''
+            checkUserId: '',
+            showSearch: false,
+            searchQuery: ''
         }
     },
     props: ['userId'],
@@ -70,6 +97,15 @@ export default {
             }
 
             document.body.classList.toggle('overflow-hidden')
+        },
+        toggleSearch() {
+            this.showSearch = !this.showSearch
+        },
+        searchAnime() {
+            // console.log('search anime')
+            // console.log(this.searchQuery)
+            this.showSearch = false
+            this.$router.push(`/search/${this.searchQuery}`)
         }
     }
 }
---
+++ pages/search/[anime].vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container px-4 mx-auto min-h-screen">
+        <div class="text-white my-4 min-h-[20vh] flex items-center">
+            <h1 class="text-2xl font-extrabold">
+                Search Result for: <br>
+                <span class="text-purple-500"><i>{{ query }}</i></span>
+            </h1>
+        </div>
+
+        <div v-if="animeList != null" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+            <div class="mb-5 relative" v-for="anime in animeList" :key="anime">
+                <a :href="'/animes/' + anime.id" class="relative">
+                    <div class="">
+                        <img :src="anime.image" alt="" class="rounded-xl object-cover w-full h-full">
+                    </div>
+                    <div class="relative flex justify-between items-center ">
+                        <div class="px-3 py-2 rounded-full bg-purple-500 text-white absolute left-1 bottom-1">
+                            <h3 class="truncate text-xs lg:text-sm max-w-[8rem] lg:max-w-xs">
+                                {{ anime.title }}
+                            </h3>
+                        </div>
+                        <span class="bg-white rounded-md text-sm px-2 absolute bottom-1 right-3"
+                            v-if="anime.episode && anime.episodeNumber">
+                            EP {{ anime.episode ?? anime.episodeNumber }}
+                        </span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div v-else class="text-white">
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: '',
+            animeList: []
+        }
+    },
+    mounted() {
+        this.query = this.$route.params.anime
+
+        useHead({
+            title: 'Search Result for: ' + this.query,
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Search Result for: ' + this.query
+                }
+            ]
+        })
+        const config = useRuntimeConfig()
+        var url = ''
+        if (localStorage.getItem('server') == 'gogoanime') {
+            url = config.apiUrl + this.query
+        } else {
+            url = config.apiUrl2 + this.query
+        }
+        this.searchAnime(url)
+
+    },
+    methods: {
+        searchAnime(api) {
+            fetch(api)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.animeList = data.results
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
