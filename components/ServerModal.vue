aac5932605961f30f583d565342c0532e9582944
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-16 22:46:11 +0800

    Update ServerModal.vue

--- components/ServerModal.vue
+++ components/ServerModal.vue
@@ -3,11 +3,11 @@
         <div>
             <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
                 leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-black bg-opacity-50" />
+                <div class="fixed inset-0 bg-black bg-opacity-50" @click="toggleModal" />
             </TransitionChild>
 
             <div class="fixed inset-0 overflow-y-auto z-50">
-                <div class="flex min-h-full items-center justify-center p-4 text-center" @click="toggleModal">
+                <div class="flex min-h-full items-center justify-center p-4 text-center">
                     <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
                         enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
                         leave-to="opacity-0 scale-95">
@@ -75,7 +75,7 @@ function toggleModal() {
 
 function setServer(server) {
     // console.log('setServer', server)
-    localStorage.setItem('server', server)
     activeServer = server
+    localStorage.setItem('server', server)
 }
 </script>
