413412b932638f147c4d9f0336af115550ddc467
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-24 15:13:49 +0800

    Update continue-watching.vue

--- pages/continue-watching.vue
+++ pages/continue-watching.vue
@@ -5,7 +5,7 @@
                 Continue <br>
                 <span class="text-purple-500 flex items-center gap-3">Watching
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                        class=" w-8 h-8 animate-bounce">
+                        class=" w-8 h-8 animate-bounce-lr">
                         <path
                             d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" />
                     </svg>
@@ -116,7 +116,12 @@ export default {
 </script>
 
 <style>
-@keyframes bounce {
+
+.animate-bounce-lr {
+    animation: bounce-lr 1s infinite;
+}
+
+@keyframes bounce-lr {
 
     0%,
     100% {
