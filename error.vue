f0b06dae4716e7aa1bd6027a5b8f365a5880b58c
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-27 20:55:33 +0800

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="bg-slate-900">
         <!-- generate error page -->
-        <div class="flex flex-col items-center justify-center h-screen">
-            <div class="text-9xl font-bold text-gray-400 mb-10">404</div>
-            <div class="text-2xl font-bold text-gray-400">Page not found</div>
-        </div>
+        <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+            <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+            <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+                Page Not Found
+            </div>
+            <button class="mt-5">
+                <a
+                    class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+                    <span
+                        class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+                    <span class="relative block px-8 py-3 bg-[#1A2238] border border-current">
+                        <router-link to="/">Go Home</router-link>
+                    </span>
+                </a>
+            </button>
+        </main>
     </div>
 </template>
 
