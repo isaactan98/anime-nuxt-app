51599bdd5ef757cd577df867577fc31a88404464
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-03-15 22:24:44 +0800

    update: v1 main page

---
+++ components/AnimeCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-for="anime in animeInfo" :key="anime" class="relative mb-2">
+        <a :href="'/v1/animes/' + anime.animeId" class="relative">
+            <div class="">
+                <img :src="anime.animeImg" loading="lazy" alt="" class="rounded-xl object-cover h-64 lg:h-[32rem]">
+            </div>
+            <div class="relative flex justify-between items-center ">
+                <div class="w-full h-32 absolute bottom-0 bg-gradient-to-t from-black to-transparent rounded-b-xl">
+                </div>
+                <div class="px-2 py-1 rounded-md bg-purple-500 text-white absolute left-1 bottom-1">
+                    <h3 class="truncate text-xs lg:text-sm max-w-[4rem] lg:max-w-[10rem]">
+                        {{ anime.animeTitle }}
+                    </h3>
+                </div>
+                <span class="bg-white rounded-md text-sm px-2 absolute bottom-1 right-3"
+                    v-if="anime.episode || anime.episodeNum">
+                    EP {{ anime.episode ?? anime.episodeNum }}
+                </span>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['animeInfo'],
+}
+</script>
+
+<style></style>
--- pages/v1/index.vue
+++ pages/v1/index.vue
@@ -1,8 +1,27 @@
 <template>
     <div class="container px-4 mx-auto min-h-screen">
         <div class="my-5">
-            <div class="flex justify-between text-white">
-                <NuxtLink to="/v1/recent-release" class=" text-white">Recent Release</NuxtLink>
+            <div class="flex justify-between text-white mb-3">
+                <h5 class="font-bold">Recent Release</h5>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                <AnimeCard :animeInfo="recentRelease"></AnimeCard>
+            </div>
+        </div>
+        <div class="my-5">
+            <div class="flex justify-between text-white mb-3">
+                <h5 class="font-bold">Top Airing</h5>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                <AnimeCard :animeInfo="topAiring"></AnimeCard>
+            </div>
+        </div>
+        <div class="my-5">
+            <div class="flex justify-between text-white mb-3">
+                <h5 class="font-bold">Popular</h5>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                <AnimeCard :animeInfo="popular"></AnimeCard>
             </div>
         </div>
     </div>
@@ -10,7 +29,91 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            recentRelease: null,
+            topAiring: null,
+            popular: null,
+            loading: [true, true, true]
+        }
+    },
+    mounted() {
+        useHead({
+            title: 'Recent Release',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Recent Release'
+                },
+                {
+                    name: 'keywords',
+                    content: 'Recent Release'
+                }
+            ]
+        })
+        this.getRecentRelease()
+        this.getTopAiring()
+        this.getPopular()
+    },
+    methods: {
+        async getRecentRelease() {
+            const config = useRuntimeConfig();
+            await fetch(config.apiUrlV1 + 'recent-release')
+                .then(response => response.json())
+                .then(data => {
+                    // console.log("data: ", data)
+                    this.loading[0] = false
+                    if (data.length > 0) {
+                        this.recentRelease = data
+                    } else {
+                        alert(data.message)
+                    }
+                    // document.getElementById('releaseHeader').scrollIntoView()
+                })
+                .catch(err => {
+                    alert('Something went wrong, please try again later')
+                    console.log(err)
+                });
+        },
+        async getTopAiring() {
+            const config = useRuntimeConfig();
+            await fetch(config.apiUrlV1 + 'top-airing')
+                .then(response => response.json())
+                .then(data => {
+                    // console.log("data: ", data)
+                    this.loading[1] = false
+                    if (data.length > 0) {
+                        this.topAiring = data
+                    } else {
+                        alert(data.message)
+                    }
+                    // document.getElementById('releaseHeader').scrollIntoView()
+                })
+                .catch(err => {
+                    alert('Something went wrong, please try again later')
+                    console.log(err)
+                });
+        },
+        async getPopular() {
+            const config = useRuntimeConfig();
+            await fetch(config.apiUrlV1 + 'popular')
+                .then(response => response.json())
+                .then(data => {
+                    // console.log("data: ", data)
+                    this.loading[2] = false
+                    if (data.length > 0) {
+                        this.popular = data
+                    } else {
+                        alert(data.message)
+                    }
+                    // document.getElementById('releaseHeader').scrollIntoView()
+                })
+                .catch(err => {
+                    alert('Something went wrong, please try again later')
+                    console.log(err)
+                });
+        }
+    }
 }
 </script>
 
