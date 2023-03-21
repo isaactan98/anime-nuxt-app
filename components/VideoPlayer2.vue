f9317cba14d12dcb561f421bdc61d71335c97640
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-03-21 22:37:40 +0800

    Update VideoPlayer2.vue
    new cors site

--- components/VideoPlayer2.vue
+++ components/VideoPlayer2.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import Artplater from 'artplayer'
-import Hls from 'hls.js'
+import Artplater from "artplayer";
+import Hls from "hls.js";
 export default {
     data() {
         return {
@@ -18,37 +18,57 @@ export default {
                 480: { url: "", quality: "480" },
                 360: { url: "", quality: "360" },
                 auto: { url: "", quality: "auto" },
-                backup: { url: "", quality: "backup" }
+                backup: { url: "", quality: "backup" },
             },
-        }
+        };
     },
-    props: ['videoDetails', 'info'],
+    props: ["videoDetails", "info"],
     mounted() {
+        this.details = { ...this.videoDetails };
+        let corsUrl = "";
 
-        this.details = { ...this.videoDetails }
-        let corsUrl = ""
-
-        fetch("https://cors.dekianime.site/").then(res => {
-            corsUrl = "https://cors.dekianime.site/"
-            console.log("OK:", res)
-        }).catch(err => {
-            // alert("Error: " + err)
-            console.log("Error", err)
-            corsUrl = ''
-        })
-
-        if (this.videoDetails.sources) {
-            // console.log("video detail source: ", this.videoDetails.sources)
-            var source = this.details.sources
-            this.url.auto.url = this.filterFilter(source, { quality: "default" })[0] ? this.filterFilter(source, { quality: "default" })[0].url : this.filterFilter(source, { quality: "auto" })[0].url
-            this.url[1080].url = this.filterFilter(source, { quality: "1080p" })[0] ? this.filterFilter(source, { quality: "1080p" })[0].url : this.url.auto.url
-            this.url[720].url = this.filterFilter(source, { quality: "720p" })[0] ? this.filterFilter(source, { quality: "720p" })[0].url : this.url.auto.url
-            this.url[480].url = this.filterFilter(source, { quality: "480p" })[0] ? this.filterFilter(source, { quality: "480p" })[0].url : this.url.auto.url
-            this.url[360].url = this.filterFilter(source, { quality: "360p" })[0] ? this.filterFilter(source, { quality: "360p" })[0].url : this.url.auto.url
-            this.url.backup.url = this.filterFilter(source, { quality: "backup" })[0] ? this.filterFilter(source, { quality: "backup" })[0].url : this.url.auto.url
+        fetch("https://cors-anywhere-lkdy.onrender.com/")
+            .then((res) => {
+                corsUrl = "https://cors-anywhere-lkdy.onrender.com/";
+                // console.log("OK:", res)
+                this.displayVideo(corsUrl);
+            })
+            .catch((err) => {
+                // alert("Error: " + err)
+                console.log("Error", err);
+                corsUrl = "";
+                this.displayVideo(corsUrl);
+            });
+    },
+    methods: {
+        filterFilter(obj, exp) {
+            return obj.filter(
+                (item) => item[Object.keys(exp)[0]] == Object.values(exp)[0]
+            );
+        },
+        displayVideo(corsUrl) {
+            var source = this.details.sources;
+            this.url.auto.url = this.filterFilter(source, { quality: "default" })[0]
+                ? this.filterFilter(source, { quality: "default" })[0].url
+                : this.filterFilter(source, { quality: "auto" })[0].url;
+            this.url[1080].url = this.filterFilter(source, { quality: "1080p" })[0]
+                ? this.filterFilter(source, { quality: "1080p" })[0].url
+                : this.url.auto.url;
+            this.url[720].url = this.filterFilter(source, { quality: "720p" })[0]
+                ? this.filterFilter(source, { quality: "720p" })[0].url
+                : this.url.auto.url;
+            this.url[480].url = this.filterFilter(source, { quality: "480p" })[0]
+                ? this.filterFilter(source, { quality: "480p" })[0].url
+                : this.url.auto.url;
+            this.url[360].url = this.filterFilter(source, { quality: "360p" })[0]
+                ? this.filterFilter(source, { quality: "360p" })[0].url
+                : this.url.auto.url;
+            this.url.backup.url = this.filterFilter(source, { quality: "backup" })[0]
+                ? this.filterFilter(source, { quality: "backup" })[0].url
+                : this.url.auto.url;
 
             this.player = new Artplater({
-                container: '.artplayer-app',
+                container: ".artplayer-app",
                 url: corsUrl + this.url.auto.url,
                 poster: this.info.image,
                 title: this.info.title,
@@ -60,89 +80,84 @@ export default {
                 fullscreenWeb: true,
                 miniProgressBar: true,
                 autoOrientation: true,
-                type: 'm3u8',
+                type: "m3u8",
                 customType: {
                     m3u8: playM3u8,
                 },
                 quality: [
                     {
-                        html: '1080p',
+                        html: "1080p",
                         url: corsUrl + this.url[1080].url,
                     },
                     {
-                        html: '720p',
+                        html: "720p",
                         url: corsUrl + this.url[720].url,
                     },
                     {
-                        html: '480p',
+                        html: "480p",
                         url: corsUrl + this.url[480].url,
                     },
                     {
-                        html: '360p',
+                        html: "360p",
                         url: corsUrl + this.url[360].url,
                     },
                     {
                         default: true,
-                        html: 'Auto',
+                        html: "Auto",
                         url: corsUrl + this.url.auto.url,
                     },
                     {
-                        html: 'Backup',
+                        html: "Backup",
                         url: corsUrl + this.url.backup.url,
                     },
                 ],
                 icons: {
                     loading: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgba(241, 242, 243, 0); display: block;" width="111px" height="111px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-<g>
-  <circle cx="60" cy="50" r="4" fill="#fae141">
-    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="-0.67s"></animate>
-    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="-0.67s"></animate>
-  </circle>
-  <circle cx="60" cy="50" r="4" fill="#fae141">
-    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="-0.33s"></animate>
-    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="-0.33s"></animate>
-  </circle>
-  <circle cx="60" cy="50" r="4" fill="#fae141">
-    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="0s"></animate>
-    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="0s"></animate>
-  </circle>
-</g><g transform="translate(-15 0)">
-  <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#9b4bd4" transform="rotate(90 50 50)"></path>
-  <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#9b4bd4">
-    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
-  </path>
-  <path d="M50 50L20 50A30 30 0 0 1 80 50Z" fill="#9b4bd4">
-    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;-45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
-  </path>
-</g>
-</svg>`,
+                                <g>
+                                <circle cx="60" cy="50" r="4" fill="#fae141">
+                                    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="-0.67s"></animate>
+                                    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="-0.67s"></animate>
+                                </circle>
+                                <circle cx="60" cy="50" r="4" fill="#fae141">
+                                    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="-0.33s"></animate>
+                                    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="-0.33s"></animate>
+                                </circle>
+                                <circle cx="60" cy="50" r="4" fill="#fae141">
+                                    <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1" begin="0s"></animate>
+                                    <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1" keyTimes="0;0.2;1" begin="0s"></animate>
+                                </circle>
+                                </g><g transform="translate(-15 0)">
+                                <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#9b4bd4" transform="rotate(90 50 50)"></path>
+                                <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#9b4bd4">
+                                    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
+                                </path>
+                                <path d="M50 50L20 50A30 30 0 0 1 80 50Z" fill="#9b4bd4">
+                                    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;-45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
+                                </path>
+                                </g>
+                                </svg>`,
                 },
-            })
-        }
+            });
 
-        function playM3u8(video, url, art) {
-            if (Hls.isSupported()) {
-                const hls = new Hls();
-                hls.loadSource(url);
-                hls.attachMedia(video);
+            function playM3u8(video, url, art) {
+                if (Hls.isSupported()) {
+                    const hls = new Hls();
+                    hls.loadSource(url);
+                    hls.attachMedia(video);
 
-                // optional
-                art.hls = hls;
-                art.once('url', () => hls.destroy());
-                art.once('destroy', () => hls.destroy());
-            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.src = url;
-            } else {
-                art.notice.show = 'Unsupported playback format: m3u8';
+                    // optional
+                    art.hls = hls;
+                    art.once("url", () => hls.destroy());
+                    art.once("destroy", () => hls.destroy());
+                } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+                    video.src = url;
+                } else {
+                    art.notice.show = "Unsupported playback format: m3u8";
+                }
             }
-        }
-    },
-    methods: {
-        filterFilter(obj, exp) {
-            return obj.filter((item) => item[Object.keys(exp)[0]] == Object.values(exp)[0])
         },
-    }
-}
+    },
+};
 </script>
 
 <style>
