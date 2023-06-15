33102618163b7ec0892b60bf84adf53601d6c5f3
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-06-15 22:50:27 +0800

    Update [news].vue

--- pages/news/[news].vue
+++ pages/news/[news].vue
@@ -15,6 +15,13 @@
                 View Source
             </a>
         </div>
+        <div class="" v-if="news === null">
+            <div class=" w-2/3 bg-purple-800 bg-opacity-25 rounded-xl h-72 animate-pulse"></div>
+            <!-- few line -->
+            <div class=" w-full bg-purple-800 bg-opacity-25 rounded-xl h-10 animate-pulse mt-3"></div>
+            <div class=" w-full bg-purple-800 bg-opacity-25 rounded-xl h-10 animate-pulse mt-3"></div>
+            <div class=" w-full bg-purple-800 bg-opacity-25 rounded-xl h-10 animate-pulse mt-3"></div>
+        </div>
     </div>
 </template>
 
@@ -29,13 +36,13 @@ export default {
     async mounted() {
         const config = useRuntimeConfig()
         await this.getNews(config.newsApi, this.$route.params, this.$route.query)
-        await this.getRecent(config.newsApi)
+        // await this.getRecent(config.newsApi)
 
-        const date = this.news.id.split('/')[0]
-        const newsId = this.news.id.split('/')[1]
-        const uuid = this.news.id.split('/')[2]
+        // const date = this.news.id.split('/')[0]
+        // const newsId = this.news.id.split('/')[1]
+        // const uuid = this.news.id.split('/')[2]
 
-        this.news.image = this.recentNews.filter(news => news.id === date + '/' + newsId + '/' + uuid)[0].thumbnail
+        // this.news.image = this.recentNews.filter(news => news.id === date + '/' + newsId + '/' + uuid)[0].thumbnail
     },
     methods: {
         async getNews(api, params, query) {
@@ -43,9 +50,10 @@ export default {
                 .then(response => response.json())
                 .then(async json => {
                     this.news = json
-                    await useNewScapper(this.news.url).then((data) => {
+                    await useNewScapper(this.news.url).then(async (data) => {
                         console.log("data:", data)
                         document.getElementById('newBody').appendChild(data.main)
+                        await this.getRecent(api)
                     })
                 })
         },
@@ -89,5 +97,9 @@ img {
 table {
     font-size: 0.8rem;
     margin: 1rem 0;
+}
+
+.fright {
+    @apply pb-3
 }
 </style>
