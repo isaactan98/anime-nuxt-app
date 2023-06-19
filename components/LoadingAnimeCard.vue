e1ccffe7849b10906c25df5f9ab72e854cf3e2cf
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-06-19 23:03:38 +0800

    Update LoadingAnimeCard.vue

--- components/LoadingAnimeCard.vue
+++ components/LoadingAnimeCard.vue
@@ -1,4 +1,5 @@
 <template>
-    <div class="rounded-2xl object-cover min-w-[220px] lg:min-w-[380px] h-64 lg:h-[32rem] bg-purple-800 animate-pulse">
+    <div
+        class="rounded-2xl object-cover min-w-[220px] lg:min-w-[380px] h-64 lg:h-[32rem] bg-purple-800 bg-opacity-40 animate-pulse">
     </div>
 </template>
