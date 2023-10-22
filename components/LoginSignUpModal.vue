d10dd6759c0b31103c72eda594b0ed8330d9bc93
Isaac Tan <iztan98@gmail.com>
2023-10-22 23:07:14 +0800

    update: login-signup model changes, reset password feature

--- components/LoginSignUpModal.vue
+++ components/LoginSignUpModal.vue
@@ -6,10 +6,10 @@
         <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0" enter-to-class="opacity-100"
             leave-active-class="ease-in duration-200" leave-from-class="opacity-100" leave-to-class="opacity-0">
             <div v-show="isOpen">
-                <div class="fixed inset-0 bg-black bg-opacity-50" @click="closeModal"></div>
-                <div class="fixed inset-0 overflow-y-auto z-50">
-                    <div class="flex min-h-full items-center justify-center p-4 text-center md:w-1/3 mx-auto">
-                        <div v-if="login" class="bg-slate-800 rounded-lg shadow-lg p-4 w-full">
+                <div class="fixed inset-0 min-h-screen bg-black bg-opacity-50" @click="closeModal"></div>
+                <div class="fixed inset-0 z-50">
+                    <div class="flex min-h-screen items-center justify-center p-4 text-center md:w-1/3 mx-auto">
+                        <div v-if="formSelection == 'login'" class="bg-slate-800 rounded-lg shadow-lg p-4 w-full">
                             <div class="flex justify-between">
                                 <h1 class="text-2xl font-bold text-white">Login</h1>
                                 <button class="text-2xl font-bold text-white" @click="closeModal">
@@ -39,16 +39,19 @@
                                 </button>
                             </div>
                             <div class="mt-4">
-                                <p class="text-sm text-white">Don't have an account? <span @click="login = false"
-                                        class="text-violet-700 cursor-pointer">
+                                <p class="text-sm text-white">Don't have an account? <span @click="formSelection = 'signup'"
+                                        class="text-violet-500 cursor-pointer">
                                         Sign Up
                                     </span></p>
+                                <button class="text-white mt-3" @click="formSelection = 'reset'">
+                                    <p class="text-sm">Reset Password?</p>
+                                </button>
                             </div>
                             <div v-if="errorMessage" class="w-full text-center text-red-500 mt-4 text-sm">
                                 {{ errorMessage }}
                             </div>
                         </div>
-                        <div v-else class="bg-slate-800 rounded-lg shadow-lg p-4 w-full">
+                        <div v-if="formSelection == 'signup'" class="bg-slate-800 rounded-lg shadow-lg p-4 w-full">
                             <div class="flex justify-between">
                                 <h1 class="text-2xl font-bold text-white">Sign Up</h1>
                                 <button class="text-2xl font-bold text-white" @click="closeModal">
@@ -77,32 +80,68 @@
                                 </button>
                             </div>
                             <div class="mt-4">
-                                <p class="text-sm text-white">Already have an account? <span @click="login = true"
-                                        class="text-violet-700 cursor-pointer">Login</span></p>
+                                <p class="text-sm text-white">Already have an account? <span
+                                        @click="formSelection = 'login'" class="text-violet-500 cursor-pointer">Login</span>
+                                </p>
                             </div>
                             <div v-if="errorMessage" class="w-full text-center text-red-500 mt-4 text-sm">
                                 {{ errorMessage }}
+                            </div>
+                        </div>
+                        <div v-if="formSelection == 'reset'" class="bg-slate-800 rounded-lg shadow-lg p-4 w-full">
+                            <div class="flex justify-between">
+                                <h1 class="text-2xl font-bold text-white">Login</h1>
+                                <button class="text-2xl font-bold text-white" @click="closeModal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="mt-4">
+                                <label for="email" class="block text-sm font-bold text-white">Email</label>
+                                <input type="email" name="email" id="email" v-model="email"
+                                    class="w-full border border-gray-300 rounded-lg px-4 py-2 mt-2 focus:outline-none focus:border-violet-700"
+                                    autocomplete="off" placeholder="Email for password reset">
+                            </div>
+                            <div class="mt-4 text-center">
+                                <button
+                                    class="bg-violet-700 text-white px-4 py-2 rounded-lg text-sm flex gap-1 justify-center mx-auto"
+                                    @click="resetPassword">
+                                    <SpiningLoading v-if="isClicked"></SpiningLoading>
+                                    Reset Password
+                                </button>
+                                <p class="text-sm mt-4 text-white">Already have an account? <span
+                                        @click="formSelection = 'login'" class="text-violet-700 cursor-pointer">Login</span>
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </transition>
+        <transition enter-active-class="transition duration-300 ease-out" enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-out"
+            leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+            <ErrorToast v-if="success" :type="'success'" :message="'Password reset email has been sent.'" />
+        </transition>
     </div>
 </template>
 
 <script>
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 
 export default {
     data() {
         return {
+            formSelection: 'login',
             login: true,
             isOpen: false,
             email: '',
             password: '',
             errorMessage: '',
-            isClicked: false
+            isClicked: false,
+            success: false
         }
     },
     props: {
@@ -172,11 +211,25 @@ export default {
                     this.errorMessage = errorMessage
                     this.isClicked = false
                 });
+        },
+        resetPassword() {
+            this.isClicked = true
+            console.log('reset password')
+            const auth = getAuth();
+            sendPasswordResetEmail(auth, this.email).then((s) => {
+                this.isClicked = false
+                this.success = true
+                setTimeout(() => {
+                    this.success = false
+                }, 3000);
+                console.log(s)
+            }).catch((e) => {
+                this.isClicked = false
+                console.error(e)
+            })
         }
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
