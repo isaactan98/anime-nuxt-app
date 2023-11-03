f07ee291192e049eb7aa2b4f786a7074e7c2b103
Isaac Tan <iztan98@gmail.com>
2023-11-03 20:20:00 +0800

    Create index.vue

---
+++ pages/anify/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="min-h-screen grid grid-cols-2 gap-3">
+        <div v-for="anime in animeList" :key="anime">
+            <img :src="anime.coverImage" alt="">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            anifyUrl: "",
+            animeList: [] as any[]
+        }
+    },
+    mounted() {
+        fetch(this.anifyUrl + "eminence").then(res => {
+            return res.json()
+        }).then(res => {
+            console.log(res)
+            if (res.length > 0) {
+                this.animeList = res
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+}
+</script>
