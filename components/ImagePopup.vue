5fd5d7fe3f0c6f5a42ba9c86c75b405e34322028
Isaac Tan <iztan98@gmail.com>
2023-11-05 12:50:21 +0800

    update: popup style

--- components/ImagePopup.vue
+++ components/ImagePopup.vue
@@ -3,14 +3,14 @@
         enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-out"
         leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
         <div v-if="open" class="relative z-50" id="headlessui-dialog-146" role="dialog" aria-modal="true">
-            <div class="fixed inset-0 transition-opacity bg-gray-200/75 dark:bg-gray-800/75" @click="close()"></div>
+            <div class="fixed inset-0 transition-opacity bg-gray-800/75" @click="close()"></div>
             <div class="fixed inset-0 overflow-y-auto">
                 <div class="flex min-h-full items-center justify-center text-center p-4 sm:p-0" @click="close()">
                     <div
-                        class="relative text-left rtl:text-right overflow-hidden w-full flex flex-col bg-white dark:bg-gray-900 shadow-xl sm:max-w-lg rounded-lg sm:my-8">
+                        class="relative text-left rtl:text-right overflow-hidden w-full flex flex-col bg-gray-900 shadow-xl sm:max-w-lg rounded-lg sm:my-8">
                         <div class="p-4">
                             <div
-                                class="relative overflow-hidden rounded border border-dashed border-gray-400 dark:border-gray-500 opacity-75 px-4 flex items-center justify-center max-h-screen">
+                                class="relative overflow-hidden rounded border border-dashed border-gray-500 px-4 flex items-center justify-center max-h-screen">
                                 <img :src="image" class="rounded-lg h-full w-full">
                             </div>
                         </div>
