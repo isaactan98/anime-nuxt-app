f6a6aed1c4668f8eba985c67102ae4259584c427
Isaac Tan <iztan98@gmail.com>
2023-12-12 07:42:17 +0800

    Update index.vue

--- pages/secret/index.vue
+++ pages/secret/index.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="min-h-screen container mx-auto px-5 mt-5 relative">
+    <div class="min-h-screen container mx-auto p-4 mt-5 relative">
         <div v-if="list.length > 0" class="grid grid-cols-2 gap-5">
-            <button v-for="(item, index) in list" :key="index" class="" @click="getVideo(item.id)">
-                <div class="min-w-[220px] lg:min-w-[380px]">
-                    <img :src="item.img" loading="lazy" alt=""
-                        class="rounded-xl object-cover w-56 lg:w-96 h-72 lg:h-[32rem]">
-                    <!-- <div class="bg-white p-10"></div> -->
-                </div>
-            </button>
+            <div v-for="(item, index) in list" :key="index" class=" relative mb-3">
+                <button @click="getVideo(item.id)" class="relative w-full">
+                    <div class="object-cover h-56 lg:h-96">
+                        <img :src="item.img" loading="lazy" alt="" class="rounded-xl object-cover max-w-full w-full h-full">
+                        <!-- <div class="bg-white p-10"></div> -->
+                    </div>
+                </button>
+            </div>
         </div>
         <div v-if="hanime.episodes.length > 0" class="mx-auto min-h-screen w-full bg-black absolute top-0">
-            <iframe :src="hanime.episodes" frameborder="0"></iframe>
+            <iframe :src="hanime.episodes" frameborder="0" class=""></iframe>
         </div>
     </div>
 </template>
