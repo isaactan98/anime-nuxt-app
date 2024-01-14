6e49fef288204c9bf336f1739aca1f713351a9c9
Isaac Tan <iztan98@gmail.com>
2024-01-14 12:53:49 +0800

    update: new version homepage

--- components/RecentReleaseComponent.vue
+++ components/RecentReleaseComponent.vue
@@ -3,18 +3,18 @@
         <div v-if="release != null" class="mb-5 relative">
             <button class="relative" @click="startLoading">
                 <TransLoading v-if="clickLoading.getLoading()" />
-                <div class="min-w-[220px] lg:min-w-[380px]">
+                <div class="min-w-[150px] lg:min-w-[220px]">
                     <img :src="release.image" loading="lazy" alt=""
-                        class="rounded-xl object-cover w-56 lg:w-96 h-72 lg:h-[32rem]">
+                        class="rounded-lg object-cover w-56 lg:w-96 h-64 lg:h-[16rem]">
                 </div>
-                <div class="relative flex justify-between items-center min-w-[230px] lg:min-w-[380px]">
-                    <div class="w-full h-32 absolute bottom-0 bg-gradient-to-t from-black to-transparent rounded-b-xl">
+                <div class="relative flex justify-between items-center min-w-[150px] lg:min-w-[220px]">
+                    <div class="w-full h-32 absolute bottom-0 bg-gradient-to-t from-black to-transparent rounded-b-lg">
                     </div>
-                    <div class="px-2 py-1 rounded-md bg-purple-500 text-white absolute left-1 bottom-1">
+                    <!-- <div class="px-2 py-1 rounded-md bg-purple-500 text-white absolute left-1 bottom-1">
                         <h3 class="truncate text-xs lg:text-sm max-w-[10rem] lg:max-w-xs">
                             {{ release.title }}
                         </h3>
-                    </div>
+                    </div> -->
                     <span class="bg-white rounded-md text-sm px-2 absolute bottom-1 right-3"
                         v-if="release.episode || release.episodeNumber">
                         EP {{ release.episode ?? release.episodeNumber }}
