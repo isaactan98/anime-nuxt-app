f1ecd4857fda795df5acd2653067b3775f8bc8bf
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2025-04-26 10:08:22 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,48 +14,40 @@ export default defineNuxtConfig({
             ],
             meta: [
                 {charset: "utf-8"},
-                {name: "viewport", content: "width=device-width; initial-scale=1; viewport-fit=cover"},
+                {name: "viewport", content: "width=device-width, initial-scale=1, viewport-fit=cover"},
                 {name: "theme-color", content: "#0d0d12"},
                 {
-                    hid: "description",
                     name: "description",
                     content:
                         "Full list of the Most Popular and Must Watch Anime Online Free, with SUBBED and DUBBED. WATCH NOW!",
                 },
                 {
-                    hid: "og:title",
                     property: "og:title",
                     content: "Shadow Anime - Shadow Anime",
                 },
                 {
-                    hid: "og:description",
                     property: "og:description",
                     content:
                         "Full list of the Most Popular and Must Watch Anime Online Free, with SUBBED and DUBBED. WATCH NOW!",
                 },
                 {
-                    hid: "og:image",
                     property: "og:image",
                     content:
                         "https://static.wikia.nocookie.net/to-be-a-power-in-the-shadows/images/f/f7/Shadow-Garden-Anime.jpg/revision/latest?cb=20221031135138",
                 },
                 {
-                    hid: "robots",
                     name: "robots",
                     content: "index, follow",
                 },
                 {
-                    hid: "mobile-web-app-capable",
                     name: "mobile-web-app-capable",
                     content: "yes",
                 },
                 {
-                    hid: "apple-mobile-web-app-capable",
                     name: "apple-mobile-web-app-capable",
                     content: "yes",
                 },
                 {
-                    hid: "apple-mobile-web-app-status-bar-style",
                     name: "apple-mobile-web-app-status-bar-style",
                     content: "black-translucent",
                 }
@@ -115,10 +107,5 @@ export default defineNuxtConfig({
         preference: 'dark',
         fallback: 'dark'
     },
-    ui: {
-        notifications: {
-            // Show toasts at the top right of the screen
-            position: 'top-0 bottom-[unset]'
-        }
-    }
+    ui: {}
 })
